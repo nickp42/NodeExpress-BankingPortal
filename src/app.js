9d57769e150f384a388cb32d655d1ac755e6ab98
nickp42 <113311339+nickp42@users.noreply.github.com>
2022-09-16 18:59:54 -0400

    moudle 4

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const { engine } = require('express/lib/application');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { accounts, users, writeJSON } = require('./data');
 
 
 const app = express();
@@ -12,13 +13,6 @@ app.set('view engine', "ejs");
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(express.urlencoded({extended: true}));
-
-const accountData = fs.readFileSync(`src/json/accounts.json`, {encoding: 'utf8'});
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync('src/json/users.json', {encoding: 'utf8'});
-const users = JSON.parse(userData);
-
 
 app.get('/', (req, res)=> {
     res.render('index', 
@@ -51,8 +45,7 @@ app.get('/transfer', (req, res) =>
 app.post('/transfer', (req, res)=> {
     accounts[req.body.from].balance -= req.body.amount;
     accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    let accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+    writeJSON();
     res.render('transfer', {message: 'Transfer Completed'});
 })
 
@@ -65,8 +58,7 @@ app.get('/payment', (req, res) =>
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount);
-    let accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    writeJSON()
     res.render('payment', {
         message: 'Payment Successful', 
         account: accounts.credit
